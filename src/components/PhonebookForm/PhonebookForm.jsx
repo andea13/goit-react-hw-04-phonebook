6a2760b69e61ee4fc1f584f71af72e96f07dfd6a
Oksana Dziuba <dziuba131303@gmail.com>
2023-10-07 15:05:25 +0400

    deletes unneeded patterns and titles of inputs

--- src/components/PhonebookForm/PhonebookForm.jsx
+++ src/components/PhonebookForm/PhonebookForm.jsx
@@ -37,8 +37,6 @@ const PhonebookForm = ({ onSubmit }) => {
           onChange={handleChange}
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
       </FormLabel>
@@ -50,8 +48,6 @@ const PhonebookForm = ({ onSubmit }) => {
           onChange={handleChange}
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
         />
       </FormLabel>
